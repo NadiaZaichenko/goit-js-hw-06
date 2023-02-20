b0e2ebbcfb50e4e61d1e38aa1e0ddcdb00f5ee0b
NadiaZaichenko <nadyazaichenko884@gmail.com>
2023-02-20 14:12:25 +0200

    Доопрацювання
    Сподіваюсь я правильно зрозуміла підказку. Дякую)

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,9 @@
 // Витягуваня посилання на всі li.item 
 const allItems = document.querySelectorAll('.item');
 console.log(`Number of categories: ${allItems.length}`);
-// Створення функціїї яка витягує посилання певного елемента, і його певної властивості 
-Array.prototype.forEach.call(allItems, (element) => {
-    const categoryRef = element.querySelector('h2').textContent;
+// Виклик методу перебирає items, збираючи та додаючи для кожного item посилання на назву категорії, та на кількість li в ній
+allItems.forEach((element) => {
+ const categoryRef = element.querySelector('h2').textContent;
     const elemRef = element.querySelectorAll('li').length;
     console.log(`Category: ${categoryRef} \nElement: ${elemRef}`);
- })
-
-
+})
